120276ce8cc2e14d772c91d778d493af640ec2b8
Mateusz Grzechociński <mateusz.grzechocinski@polidea.com>
2014-12-04 03:44:47 +0100

    add retrolambda

--- app/src/main/java/net/grzechocinski/android/droidconkrakow/demo9/ComposeMessageActivity.java
+++ app/src/main/java/net/grzechocinski/android/droidconkrakow/demo9/ComposeMessageActivity.java
@@ -9,13 +9,14 @@ import android.widget.Toast;
 
 import net.grzechocinski.android.droidconkrakow.R;
 import rx.Observable;
+import rx.Subscription;
 import rx.android.events.OnTextChangeEvent;
 import rx.android.observables.ViewObservable;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
 
-//https://github.com/andrewhr/rxjava-android-example
+//https://github.com/andrewhr/rxjava-android-example with modifications
 public class ComposeMessageActivity extends Activity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
@@ -31,78 +32,34 @@ public class ComposeMessageActivity extends Activity {
         final Observable<String> messageBodyText  = Events.text(messageBody);
         final Observable<Object> sendMessageClick = Events.click(sendMessage);
 
-        phoneNumberText.subscribe(new Action1<String>() {
-            @Override
-            public void call(String text) {
-                System.out.println(text);
-            }
-        });
+        phoneNumberText.subscribe(System.out::println);
 
-        // send button is only enabled when we have some message body content
         messageBodyText
-                .map(new Func1<String, Boolean>() {
-                    @Override
-                    public Boolean call(String text) {
-                        return !text.trim().equals("");
-                    }
-                })
+                .map(text -> !text.trim().equals(""))
                 .subscribe(Properties.enabledFrom(sendMessage));
 
-        // counts remaining characters of body
         final int maxBodyLength = 200;
         messageBodyText
-                .map(new Func1<String, Integer>() {
-                    @Override public Integer call(String text) {
-                        return maxBodyLength - text.length();
-                    }
-                })
-                .map(new Func1<Integer, String>() {
-                    @Override public String call(Integer remainingChars) {
-                        return getString(R.string.chars, remainingChars, maxBodyLength);
-                    }
-                })
+                .map(text -> maxBodyLength - text.length())
+                .map(remainingChars -> getString(R.string.chars, remainingChars, maxBodyLength))
                 .subscribe(Properties.textFrom(remainingCharacters));
 
-        // transforms clicks into messages, using the most recent information
-        // for messages with blank phone number take focus to phone number edit text
-        // for complete messages, we show it as a Toast and cleanup message body edit text
-        // side-effects yay!
-        sendMessageClick.subscribe(new Action1<Object>() {
-            @Override
-            public void call(Object onClickEvent) {
-                System.out.println("Click!!!! : ");
-            }
-        });
-
-
         sendMessageClick
-                .flatMap(new Func1<Object, Observable<Message>>() {
-                    @Override
-                    public Observable<Message> call(Object unused) {
-                        return Observable.combineLatest(
-                                phoneNumberText,
-                                messageBodyText,
-                                new Func2<String, String, Message>() {
-                                    @Override
-                                    public Message call(String phoneNumber, String messageBody) {
-                                        return new Message(phoneNumber, messageBody);
-                                    }
-                                })
-                                .take(1);
-                    }
-                })
-                .subscribe(new Action1<Message>() {
-                    @Override public void call(Message message) {
-                        if (message.phoneNumber.toString().trim().equals("")) {
-                            phoneNumber.requestFocus();
-                        } else {
-                            messageBody.setText("");
-                            Toast.makeText(
-                                    ComposeMessageActivity.this,
-                                    message.toString(),
-                                    Toast.LENGTH_SHORT
-                            ).show();
-                        }
+                .flatMap(unused -> Observable.combineLatest(
+                        phoneNumberText,
+                        messageBodyText,
+                        (phoneNumber1, messageBody1) -> new Message(phoneNumber1, messageBody1))
+                        .take(1))
+                .subscribe(message -> {
+                    if (message.phoneNumber.toString().trim().equals("")) {
+                        phoneNumber.requestFocus();
+                    } else {
+                        messageBody.setText("");
+                        Toast.makeText(
+                                ComposeMessageActivity.this,
+                                message.toString(),
+                                Toast.LENGTH_SHORT
+                        ).show();
                     }
                 });
     }
